caaedccdd1e18dad880fcba693742d9f333cb156
rincy <rincy@192.168.1.112>
2024-10-16 06:59:06 +0530

    aggrigation

---
+++ MavenProject/src/main/java/aggrigation/AggrigationEx1.java
@@ -0,0 +1,11 @@
+package aggrigation;
+
+public class AggrigationEx1 {
+String name;
+int rollno;
+public AggrigationEx1(String name,int rollno)
+{
+	this.name=name;
+	this.rollno=rollno;
+}
+}
---
+++ MavenProject/src/main/java/aggrigation/AggrigationEx2.java
@@ -0,0 +1,25 @@
+package aggrigation;
+
+public class AggrigationEx2 {
+String city;
+int pin;
+AggrigationEx1 ref; //aggrigation
+public AggrigationEx2(String city,int pin,AggrigationEx1 ref)
+{
+	this.city=city;
+	this.pin=pin;
+	this.ref=ref;
+}
+public void display()
+{
+	System.out.println(ref.name+" "+ref.rollno);
+	System.out.println(city+" "+pin);
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AggrigationEx1 obj=new AggrigationEx1("Rincy",12);
+		AggrigationEx2 obj1=new AggrigationEx2("kollam",691602,obj); //calling by using reference variable
+		obj1.display();
+	}
+
+}

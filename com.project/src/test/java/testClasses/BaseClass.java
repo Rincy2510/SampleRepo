72d9786cc1b7b5b298f7c43e2ce8a5a92f6544ee
rincy <rincy@LAPTOP-PL64QS0V>
2024-12-19 20:55:25 +0530

    test

--- com.project/src/test/java/testClasses/BaseClass.java
+++ com.project/src/test/java/testClasses/BaseClass.java
@@ -1,5 +1,6 @@
 package testClasses;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
 import UtilityClasses.ScreenshotClass;
 
@@ -10,11 +11,14 @@ import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestContext;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
 
 public class BaseClass {
-  WebDriver driver;
+  public static WebDriver driver;
   static Properties p;
   
   
@@ -28,24 +32,37 @@ public class BaseClass {
   	}
   @BeforeMethod
   public void beforeMethod() throws IOException {
-	  readproperty();
-	  driver=new ChromeDriver();
+	  readproperty(); 
 	  driver.get(p.getProperty("base_url"));
 	  driver.manage().window().maximize();
 	  driver.navigate().refresh();
 	  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(6));
+  }
+  @BeforeClass
+  public void beforeclass()
+  {
+	  driver=new ChromeDriver();
   }
   
 
   @AfterMethod
 	public void afterMethod(ITestResult itr) throws IOException {
 
-		if (itr.getStatus() == ITestResult.FAILURE) {
+		//if (itr.getStatus() == ITestResult.FAILURE) {
 
-			ScreenshotClass.takeScreenshot(driver, itr.getName());
+			//ScreenshotClass.takeScreenshot(driver, itr.getName());
 
-		}
+		//}
 	  driver.quit();
+  }
+  @BeforeSuite(alwaysRun = true)
+  public void createReport(final ITestContext testContext)
+  {
+	  extentReport.ExtentManager.createInstance();
+  }
+  @AfterSuite
+  public void aftersuit() {
+	  System.out.println("********Execution completed******");
   }
 
 }
